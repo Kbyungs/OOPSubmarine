99cfe1d1a89bcaa343f8619827baf514bed1b717
KBSOO <86033302+Kbyungs@users.noreply.github.com>
2024-05-25 00:38:59 +0900

    Update Map.java

--- SubmarineClient/src/Map.java
+++ SubmarineClient/src/Map.java
@@ -5,32 +5,30 @@ import java.util.Random;
 
 public class Map {
     int width;
-    int num_mine;
-    int[][] mineMap;
-    int[][] displayMap;
-    HashMap<Integer, Integer> minePosition;
+    int num_trs; //treasure
+    String [][] mineMap;
+    String [][] displayMap;
+    HashMap<Integer, Integer> trsPosition;
 
-    public Map(int width, int num_mine) {
+    public Map(int width, int num_trs) {
         this.width = width;
-        this.num_mine = num_mine;
+        this.num_trs = num_trs;
 
         // create map
         System.out.println("Create  "+ width+" X "+ width + "  map");
-        mineMap = new int[width][width];
-        displayMap = new int[width][width];
+        mineMap = new String[width][width];
+        displayMap = new String[width][width];
         for (int i=0; i<width*width; i++) {
-            mineMap[i/width][i%width] = 0;
-            displayMap[i/width][i%width] = 0;
+            mineMap[i/width][i%width] = "0";
+            displayMap[i/width][i%width] = "0";
         }
-
         printMap(mineMap);
-
     }
 
     public int checkMine(int x, int y) {
         int pos = (x*width) + y;
 
-        if (minePosition.containsValue(pos)) {
+        if (trsPosition.containsValue(pos)) {
             System.out.println("   Find mine at ("+x+", "+y+")");
             return pos;
         }
@@ -38,10 +36,9 @@ public class Map {
             System.out.println("   No mine at ("+x+", "+y+")");
             return -1;
         }
-
     }
 
-    public void printMap(int[][] a) {
+    public void printMap(String [][] a) {
         System.out.println();
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i][0]);
@@ -52,8 +49,7 @@ public class Map {
     }
 
     public void updateMap(int x, int y) {
-        displayMap[x][y]=1;
+        displayMap[x][y] = "1";
         printMap(displayMap);
     }
-
 }
