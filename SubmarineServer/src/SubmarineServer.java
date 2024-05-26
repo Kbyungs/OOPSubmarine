dc48701457e4f6227e1f667b5614cbc07c7dbc12
KBSOO <86033302+Kbyungs@users.noreply.github.com>
2024-05-27 00:25:41 +0900

    Update SubmarineServer.java

--- SubmarineServer/src/SubmarineServer.java
+++ SubmarineServer/src/SubmarineServer.java
@@ -9,6 +9,7 @@ public class SubmarineServer {
     public static int numPlayer = 0;
     public static int width = 10;
     public static int num_trs = 10;
+    public static int num_mine = 3;
     public static Map map;
 
     public static void main(String[] args) throws Exception {
@@ -33,21 +34,47 @@ public class SubmarineServer {
         }
 
         map = new Map(width, num_trs);
+
+        for (Client c : clients) {
+            for (int i = 0; i < num_mine; i++) {
+                int x = Integer.parseInt(c.mines[i][0]);
+                int y = Integer.parseInt(c.mines[i][1]);
+                map.deployMine(x, y, c.userName.substring(0, 1));
+                map.displayMap[x][y] = "s";
+            }
+        }
+
+        map.numbering();
+        map.printMap(map.mineMap);
+
         sendtoall("Start Game");
 
-        // 양 팀 지뢰 매설 시작
         while (true) {
             if (allTurn()) {
                 System.out.println();
 
                 for (Client c : clients) {
                     int check = map.checkMine(c.x, c.y);
-                    if (check >= 0) {
-                        System.out.println(c.userName + " hit at (" + c.x + " , " + c.y + ")");
-                        map.updateMap(c.x, c.y);
-                    } else
-                        System.out.println(c.userName + " miss at (" + c.x + " , " + c.y + ")");
+//                    if (check >= 0) {
+//                        System.out.println(c.userName + " hit at (" + c.x + " , " + c.y + ")");
+//                        map.updateMap(c.x, c.y);
+//                    } else
+//                        System.out.println(c.userName + " miss at (" + c.x + " , " + c.y + ")");
 
+                    if (check == 99) {  // 보물발견
+                        System.out.println(c.userName + " find treasure at (" + c.x + ", " + c.y + ")");
+                        if (c.hp < 3) c.hp++;
+                        System.out.println(c.userName + "'s hp : " + c.hp);
+                    } else if (check == 98) {// 지뢰밟음
+                        if (!map.mineMap[c.x][c.y].equals(c.userName.substring(0,1))) { //본인지뢰는 pass
+                            System.out.println(c.userName + " hit the bomb at (" + c.x + ", " + c.y + ")");
+                            c.hp--;
+                        }
+                        System.out.println(c.userName + "'s hp : " + c.hp);
+                    } else { //아무것도 못찾음
+                        System.out.println(c.userName + " miss at (" + c.x + " , " + c.y + ")");
+                    }
+                    map.updateMap(c.x,c.y);
                     c.send("" + check);
                     c.turn = true;
                 }
@@ -65,7 +92,6 @@ public class SubmarineServer {
         for (Client c : clients)
             if (!c.turn)
                 i++;
-
         return i == clients.size();
     }
 
@@ -74,7 +100,9 @@ public class SubmarineServer {
         PrintWriter out = null;
         BufferedReader in = null;
         String userName = null;
+        String[][] mines = null;
         int x, y;
+        int hp = 3;
         public boolean turn = false;
 
         public Client(Socket socket) throws Exception {
@@ -89,7 +117,7 @@ public class SubmarineServer {
             ObjectInputStream objectInput = new ObjectInputStream(socket.getInputStream());
 
             userName = (String) objectInput.readObject();
-            String[][] mines = (String[][]) objectInput.readObject(); // 2D 배열 수신
+            mines = (String[][]) objectInput.readObject(); // 2D 배열 수신
             System.out.println(userName + " joins from  " + socket.getInetAddress());
             System.out.println("수신한 지뢰 배열: ");
             for (int i = 0; i < mines.length; i++) {
